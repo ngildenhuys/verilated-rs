b7c0de32a32dfa23416f45bdec6b32e56fe82a27
Nicholas Gildenhuys <nicholas@gildenhuys.net>
2024-07-21 22:16:58 -0700

    chore: cleaning up pre-commit

--- example/rtl/top.v
+++ example/rtl/top.v
@@ -9,12 +9,12 @@ module top(
     input clk_i;
     input rst_i;
     output [3:0] count_o;
-   
+
     // 4-bit counter
     counter uut(
         .clk_i(clk_i),
         .rst_i(rst_i),
         .count_o(count_o)
     );
-   
+
 endmodule
